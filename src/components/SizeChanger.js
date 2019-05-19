f12942aa2a8e567ee887761d1df4e4a2bd7e7909
Eric <github@esellors.com>
2019-05-19 17:36:31 -0500

    fixed where selects will now disable or enable

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ColorChanger extends Component {
   render() {
     return (
-      <select disable={this.props.allowEdit} className="dropDownContainer" onChange={e => this.props.update(parseInt(e.target.value))}>
+      <select disabled={this.props.allowEdit} className="dropDownContainer" onChange={e => this.props.update(parseInt(e.target.value))}>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
